00c7a991fc1e434389239203fc45740fce559106
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-07-10 16:21:27 -0300

    feat: updates todo

--- src/index.js
+++ src/index.js
@@ -69,7 +69,20 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found' });
+  }
+
+  todo.title = title || todo.title;
+  todo.deadline = new Date(deadline);
+
+  return response.json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
